811a01ed90411a56a5ffc8b1bd09e655d674b44d
TGianoni <gianoni2@hotmail.com>
2025-10-07 17:56:12 -0300

    chore: add shadcn setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Button } from './components/ui/button'
+
 const App = () => {
-  return <h1 className="text-red-500">Hello world</h1>
+  return (
+    <div>
+      <h1 className="text-red-500">Hello world</h1>
+      <Button>Hello World!</Button>
+    </div>
+  )
 }
 
 export default App
