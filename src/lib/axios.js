7e00a0c4c1c793a07bdeb0b69eefb4189cd0ae72
TGianoni <gianoni2@hotmail.com>
2025-10-10 10:59:44 -0300

    fix: target to resolve cors and RENDER_URL

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+const RENDER_URL = 'https://fullstackclub-finance-dashboard-api.onrender.com'
+
 export const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: RENDER_URL + '/api',
 })
--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,13 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000', // endereço do seu backend
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
