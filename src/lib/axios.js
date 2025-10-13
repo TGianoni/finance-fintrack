a7a63d2044dca8626c04415f54fb5696eb4c9aa1
TGianoni <gianoni2@hotmail.com>
2025-10-13 10:56:12 -0300

    feat: ensure access token is attached on all api requests

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,19 @@
 import axios from 'axios'
 
+import { LOCAL_STORAGE_ACCESS_TOKEN_KEY } from '@/constants/local-storage'
+
 const RENDER_URL = 'https://fullstackclub-finance-dashboard-api.onrender.com'
 
 export const api = axios.create({
   baseURL: RENDER_URL + '/api',
+})
+
+api.interceptors.request.use((request) => {
+  const accessToken = localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY)
+  if (!accessToken) {
+    return request
+  }
+  console.log('Colocando access Token na request')
+  request.headers.Authorization = `Bearer ${accessToken}`
+  return request
 })
