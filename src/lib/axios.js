8cb050e537b9b426613f68ed877c5b845fb5ed2b
TGianoni <gianoni2@hotmail.com>
2025-10-13 11:04:24 -0300

    feat: add protected and public api instances

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,11 +4,15 @@ import { LOCAL_STORAGE_ACCESS_TOKEN_KEY } from '@/constants/local-storage'
 
 const RENDER_URL = 'https://fullstackclub-finance-dashboard-api.onrender.com'
 
-export const api = axios.create({
+export const protectedApi = axios.create({
   baseURL: RENDER_URL + '/api',
 })
 
-api.interceptors.request.use((request) => {
+export const publicApi = axios.create({
+  baseURL: RENDER_URL + '/api',
+})
+
+protectedApi.interceptors.request.use((request) => {
   const accessToken = localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY)
   if (!accessToken) {
     return request
