a58730512683fa65fa69d6641eaf66350f0af6d8
TGianoni <gianoni2@hotmail.com>
2025-10-17 10:08:04 -0300

    feat: add update transaction service and api functions

--- src/api/services/transaction.js
+++ src/api/services/transaction.js
@@ -12,7 +12,12 @@ export const TransactionService = {
    * @param { string } input.type - tipo da transação ("EARNING", "EXPENSE", "INVESTMENT")
    */
   create: async (input) => {
-    const response = await protectedApi.post('/transactions/me', input)
+    const response = await protectedApi.post('/transactions/me', {
+      name: input.name,
+      date: input.date,
+      amount: input.amount,
+      type: input.type,
+    })
     return response.data
   },
   /**
@@ -24,6 +29,24 @@ export const TransactionService = {
   getAll: async (input) => {
     const query = queryString.stringify({ from: input.from, to: input.to })
     const response = await protectedApi.get(`/transactions/me?${query}`)
+    return response.data
+  },
+  /**
+   * Atualiza uma transação para o usuário autenticado
+   * @param { Object} input - Transação a ser criada
+   * @param { string } input.id - ID da transação
+   * @param { string } input.name - Nome da transação
+   * @param { string } input.date - Data da transação
+   * @param { string } input.amount - Valor da transação
+   * @param { string } input.type - tipo da transação ("EARNING", "EXPENSE", "INVESTMENT")
+   */
+  update: async (input) => {
+    const response = await protectedApi.patch(`/transactions/me/${input.id}`, {
+      name: input.name,
+      date: input.date,
+      amount: input.amount,
+      type: input.type,
+    })
     return response.data
   },
 }
